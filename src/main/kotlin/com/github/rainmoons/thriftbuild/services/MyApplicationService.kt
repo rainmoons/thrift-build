82e1688c7d9ba35e0931f43fbb4604dcafeb5685
GitHub Action <action@github.com>
2021-07-03 06:51:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/rainmoons/thriftbuild/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rainmoons/thriftbuild/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rainmoons.thriftbuild.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rainmoons.thriftbuild.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rainmoons/thriftbuild/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rainmoons/thriftbuild/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rainmoons.thriftbuild.services
 
+import com.github.rainmoons.thriftbuild.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
